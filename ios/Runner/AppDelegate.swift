5aab57df17892bac259e418a1f203f96596defba
James Wainwright <jw3015@bath.ac.uk>
2020-02-07 22:39:18 +0000

    Add GMP API key.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBusOOdOW-E7Hx_5GjsthUShMUj_L7z8OA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
