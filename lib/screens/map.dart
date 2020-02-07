ff290cbd2d246b10dd5008996b9c54652c01da6e
James Wainwright <jw3015@bath.ac.uk>
2020-02-07 22:43:13 +0000

    Add map centred on University of Bath.

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:integrated_project/screens/map_drawer.dart';
 import 'package:integrated_project/screens/pin_info_drawer.dart';
 
@@ -9,8 +12,33 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("App Title"),
       ),
+      body: MapBody(),
       drawer: MapDrawer(),
       floatingActionButton: PinButton(),
+    );
+  }
+}
+
+class MapBody extends StatefulWidget {
+  @override
+  State<MapBody> createState() => MapBodyState();
+}
+
+class MapBodyState extends State<MapBody> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition uobPosition =
+      CameraPosition(target: LatLng(51.3782261, -2.3285874), zoom: 14.4746);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: uobPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
@@ -29,4 +57,3 @@ class PinButton extends StatelessWidget {
     );
   }
 }
-
