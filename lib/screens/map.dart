ee5ba43f295158ed098fc16bed4a584bad692880
James Wainwright <jw3015@bath.ac.uk>
2020-02-09 14:55:04 +0000

    Add app bar with buttons to map screen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -9,12 +9,44 @@ class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("App Title"),
-      ),
       body: MapBody(),
       drawer: MapDrawer(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: PinButton(),
+      resizeToAvoidBottomInset: false, // prevents map resizing with keyboard
+      extendBody: true, // puts map below the notched app bar
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: MapBarContents(),
+      ),
+    );
+  }
+}
+
+class MapBarContents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: Icon(Icons.menu),
+        ),
+        Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.gps_fixed),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ),
+      ],
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
     );
   }
 }
@@ -33,6 +65,7 @@ class MapBodyState extends State<MapBody> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      resizeToAvoidBottomInset: false, // prevents map moving with keyboard
       body: GoogleMap(
         initialCameraPosition: uobPosition,
         onMapCreated: (GoogleMapController controller) {
@@ -47,7 +80,7 @@ class PinButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      child: Icon(Icons.location_on),
+      child: Icon(Icons.add_location),
       onPressed: () {
         showBottomSheet(
           context: context,
