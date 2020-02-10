43b28b8837897dad106a5cf1825cf9d21ed98148
Pearse Moloney <pearsemoloney@gmail.com>
2020-02-10 16:04:21 +0000

    Tweaked keys so Google Maps works on iOS.
    Signed-off-by: Pearse Moloney <pearsemoloney@gmail.com>

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,8 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBusOOdOW-E7Hx_5GjsthUShMUj_L7z8OA")
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBusOOdOW-E7Hx_5GjsthUShMUj_L7z8OA")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
