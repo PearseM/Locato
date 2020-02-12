7b0c3fef05ba430d5526c9272c4591b1f65c7fc7
JakeCruttenden <48126452+JakeCruttenden@users.noreply.github.com>
2020-02-12 16:08:03 +0000

    Added Account Delete Button

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Your account"),
+        title: Text("Account Settings"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FlatButton.icon(
+          onPressed: () {
+            return Alert(
+                context: context,
+                title: "WARNING: DELETE ACCOUNT",
+                desc: "This cannot be undone. Would you still like to continue?",
+                buttons: [
+                  DialogButton(
+                      child: Text(
+                          "Delete",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      color: Colors.red[800],
+                      onPressed: () {
+                        //PUT CODE FOR DELETING ACCOUNT HERE
+                      },
+                  ),
+                  DialogButton(
+                    child: Text(
+                        "Cancel",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ]
+            ).show();
+          },
+          icon: Icon(
+          Icons.delete_forever,
+          size: 70.0,
+          color: Colors.red[800],
+          ),
+          color: Colors.lightBlue,
+          label: Text(
+              "DELETE ACCOUNT",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              //color: Colors.red[800],
+            ),
+          ),
+        ),
       ),
     );
   }
