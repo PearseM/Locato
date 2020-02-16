45d53a077e2273397e277009023d0514d02ea4a6
James Wainwright <jw3015@bath.ac.uk>
2020-02-16 11:39:02 +0000

    Add description box and padding to new-pin sheet

--- lib/screens/new_pin_sheet.dart
+++ lib/screens/new_pin_sheet.dart
@@ -3,22 +3,34 @@ import 'package:flutter/material.dart';
 class NewPinSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: "Name of pin", border: UnderlineInputBorder()),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Pin must have a name';
-              }
-              return null;
-            },
-          )
-        ],
+    return Container(
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Name of pin",
+                border: UnderlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+              ),
+              validator: (value) =>
+                  value.isEmpty ? 'Pin must have a name' : null,
+            ),
+            SizedBox(height: 5.0),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Description of pin",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                //border: OutlineInputBorder(),
+              ),
+              validator: (value) =>
+                  value.isEmpty ? "Pin must have description" : null,
+              maxLines: 5,
+            )
+          ],
+        ),
       ),
     );
   }
