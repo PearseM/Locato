48ef4f21f263f3034cd98e8f01937656cd24f6c3
Kal <KT644@bath.ac.uk>
2020-02-25 11:29:30 +0000

    Login Screen with registration Data not used yet

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:integrated_project/screens/map.dart';
+import 'package:integrated_project/sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
   LoginScreenState createState() => LoginScreenState();
 }
-
 
 class LoginScreenState extends State<LoginScreen> {
   @override
@@ -23,26 +23,32 @@ class LoginScreenState extends State<LoginScreen> {
           children: <Widget>[
 
             new Container(
-              padding: EdgeInsets.symmetric(vertical: 100.0,),
+              padding: EdgeInsets.symmetric(vertical: 50.0,),
               child: new Image(
-                  image:NetworkImage('https://img.pixers.pics/pho_wat(s3:700/FO/46/67/81/42/700_FO46678142_6f739fe2238316c0650ebfab20c11f7b.jpg,250,250,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-camera-photo-lens-with-earth-globe-inside-vector.jpg.jpg')
+                  image:NetworkImage('https://img.pixers.pics/pho_wat(s3:700/FO/46/67/81/42/700_FO46678142_6f739fe2238316c0650ebfab20c11f7b.jpg,150,150,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-camera-photo-lens-with-earth-globe-inside-vector.jpg.jpg')
               ),
             ),
 
-          new Container(
-            padding: EdgeInsets.symmetric(vertical: 5.0,),
-            child: new RaisedButton.icon(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPageState()),
-                );
-              },
-              icon: Icon(
-                Icons.assignment,
+            new Container(
+              padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+              child: new TextField(
+                enabled: true,
+                maxLength: 20,
+                maxLengthEnforced: true,
+                decoration: InputDecoration(
+                  hintText:'Username',
+                ),
               ),
-              label: Text('Register'),
-               color: Colors.blue,
+            ),
+
+            new Container(
+              padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+              child: new TextField(
+                enabled: true,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText:'Password',
+                ),
               ),
             ),
 
@@ -52,7 +58,7 @@ class LoginScreenState extends State<LoginScreen> {
             onPressed: () {
               Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginPageState()),
+                  MaterialPageRoute(builder: (context) => MapPage()),//LoginPageState()),
               );
             },
              icon: Icon(
@@ -63,158 +69,57 @@ class LoginScreenState extends State<LoginScreen> {
            ),
           ),
 
-          ],
-        )
-    )
-    );
-  }
-}
-
-class LoginPageState extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Center(
-          child: new Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  maxLength: 20,
-                  maxLengthEnforced: true,
-                  decoration: InputDecoration(
-                    hintText:'Username',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText:'Password',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0),
-
-                child: new RaisedButton.icon(
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MapPage()),
-                    );
-                  },
-                  icon: Icon(
-                    Icons.account_circle,
-                  ),
-                  label: Text('Login'),
-                  color: Colors.blue,
-              ),
-            ),
-          ],
-        ),
-      )
-    );
-  }
-}
-
-class RegisterPageState extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Registration Page"),
-        ),
-        body: Center(
-          child: new Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  maxLength: 20,
-                  maxLengthEnforced: true,
-                  decoration: InputDecoration(
-                    hintText:'Username',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText:'Password',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText:'Confirm Password',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText:'E-mail',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                child: new TextField(
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText:'Confirm E-Mail',
-                  ),
-                ),
-              ),
-
-              new Container(
-                padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0),
-
-                child: new RaisedButton.icon(
-                  onPressed: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MapPage()),
-                    );
-                  },
-                  icon: Icon(
-                    Icons.assignment,
-                  ),
-                  label: Text('Register'),
-                  color: Colors.blue,
-                ),
-              ),
+          new Container(
+            child: Center(
+              child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                  // signInButton(context),
             ],
+            ),
           ),
-        )
-    );
+          ),
+
+            new OutlineButton(
+              splashColor: Colors.grey,
+              onPressed: () {
+                signInWithGoogle().whenComplete(() {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MapPage()
+                      )
+                  );
+                });
+              },
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+              highlightElevation: 0,
+              borderSide: BorderSide(color: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(image: NetworkImage('https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'), height: 35.0),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Sign in with Google',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+        ],
+      ),
+    ));
   }
 }
+
