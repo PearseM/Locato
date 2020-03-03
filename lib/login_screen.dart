6e4e4632bf30fd1a5b79c34afe83398b6d748c65
James Wainwright <jw3015@bath.ac.uk>
2020-03-03 17:35:24 +0000

    Change login screen UI
    Added the flutter_auth_buttons package to get the "sign in with google"
    button that follows Google's branding guidelines:
    https://developers.google.com/identity/branding-guidelines
    
    Also adds the google_fonts package to pull from fonts.google.com.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:integrated_project/resources/account.dart';
-import 'package:integrated_project/resources/database.dart';
 import 'package:integrated_project/screens/map.dart';
 import 'package:integrated_project/sign_in.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -15,115 +14,41 @@ class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("PhotoScout"),
-      ),
-      body: Center(
-        child: new Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-
-            new Container(
-              padding: EdgeInsets.symmetric(vertical: 50.0,),
-              child: new Image(
-                  image:NetworkImage('https://img.pixers.pics/pho_wat(s3:700/FO/46/67/81/42/700_FO46678142_6f739fe2238316c0650ebfab20c11f7b.jpg,150,150,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-camera-photo-lens-with-earth-globe-inside-vector.jpg.jpg')
-              ),
-            ),
-
-            new Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-              child: new TextField(
-                enabled: true,
-                maxLength: 20,
-                maxLengthEnforced: true,
-                decoration: InputDecoration(
-                  hintText:'Username',
-                ),
-              ),
-            ),
-
-            new Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-              child: new TextField(
-                enabled: true,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText:'Password',
-                ),
-              ),
-            ),
-
-          new Container(
-            padding: EdgeInsets.symmetric(vertical: 5.0,),
-            child: new RaisedButton.icon(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapPage()),//LoginPageState()),
-              );
-            },
-             icon: Icon(
-               Icons.account_circle,
-           ),
-            label: Text('Login'),
-            color: Colors.blue,
-           ),
-          ),
-
-          new Container(
-            child: Center(
-              child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  // signInButton(context),
-            ],
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment(0.0, -0.6),
+            child: Text(
+              "locato",
+              style: GoogleFonts.merriweather().copyWith(color: Colors.white),
+              textScaleFactor: 5.0,
             ),
           ),
+          Align(
+            child: Card(
+              shape: StadiumBorder(),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(Icons.beach_access, size: 250.0),
+              ),
+            ),
           ),
-
-            new OutlineButton(
-              splashColor: Colors.grey,
+          Align(
+            alignment: Alignment(0.0, 0.75),
+            child: GoogleSignInButton(
               onPressed: () {
                 SignIn().signInWithGoogle().then((user) {
                   if (user != null) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MapPage()
-                        )
-                    );
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MapPage()));
                   }
                 });
               },
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-              highlightElevation: 0,
-              borderSide: BorderSide(color: Colors.black),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image(image: NetworkImage('https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip'), height: 35.0),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        'Sign in with Google',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+            ),
+          ),
         ],
       ),
-    ));
+    );
   }
 }
-
