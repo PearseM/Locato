010a9c94eec8cbd0d1454b12b0a9c40becd1b4d6
James Wainwright <jw3015@bath.ac.uk>
2020-03-21 15:41:58 +0000

    Add progress indicator to pin-info-drawer image

--- lib/screens/pin_info_drawer.dart
+++ lib/screens/pin_info_drawer.dart
@@ -38,6 +38,72 @@ class _PinInfoDrawerState extends State<PinInfoDrawer> {
         ),
       ),
     );
+
+    Widget visitedButton = StreamBuilder<List<String>>(
+      stream: Database.visitedByUser(Account.currentAccount, context),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
+
+        return Padding(
+          padding: EdgeInsets.all(8.0),
+          child: RaisedButton(
+            child: Text("Visited"),
+            onPressed: () {
+              if (snapshot.data.contains(widget.pin.id)) {
+                Database.deleteVisited(
+                    Account.currentAccount.id, widget.pin.id);
+              } else {
+                Database.addVisited(Account.currentAccount.id, widget.pin.id);
+              }
+            },
+            shape: StadiumBorder(),
+            color: snapshot.data.contains(widget.pin.id)
+                ? Colors.green
+                : Theme.of(context).primaryColorDark,
+            textColor: Theme.of(context).primaryTextTheme.button.color,
+          ),
+        );
+      },
+    );
+
+    Widget image = GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => imageView),
+      ),
+      child: Image.network(
+        widget.imgURL,
+        loadingBuilder: (context, child, progress) {
+          if (progress == null) {
+            return child;
+          }
+          return Center(
+            child: CircularProgressIndicator(backgroundColor: Colors.white),
+          );
+        },
+        width: 50.0,
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.cover,
+      ),
+    );
+
+    Widget copyURLButton = IconButton(
+      icon: Icon(
+        Icons.content_copy,
+        semanticLabel: "Copy URL",
+      ),
+      color: Colors.white,
+      onPressed: () {
+        Clipboard.setData(
+            ClipboardData(text: widget.pin.id.hashCode.toString()));
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("URL copied to clipboard."),
+        ));
+      },
+    );
+
     return DraggableScrollableSheet(
       expand: false,
       initialChildSize: 0.75,
@@ -49,63 +115,9 @@ class _PinInfoDrawerState extends State<PinInfoDrawer> {
           title: Text(widget.pin.name),
           shape: Theme.of(context).bottomSheetTheme.shape,
           actions: <Widget>[
-            StreamBuilder<List<String>>(
-                stream: Database.visitedByUser(Account.currentAccount, context),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Container();
-                  }
-
-                  return Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: RaisedButton(
-                      child: Text("Visited"),
-                      onPressed: () {
-                        if (snapshot.data.contains(widget.pin.id)) {
-                          Database.deleteVisited(
-                              Account.currentAccount.id, widget.pin.id);
-                        } else {
-                          Database.addVisited(
-                              Account.currentAccount.id, widget.pin.id);
-                        }
-                      },
-                      shape: StadiumBorder(),
-                      color: snapshot.data.contains(widget.pin.id)
-                          ? Colors.green
-                          : Theme.of(context).primaryColorDark,
-                      textColor:
-                          Theme.of(context).primaryTextTheme.button.color,
-                    ),
-                  );
-                }),
-            GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => imageView),
-              ),
-              child: Image.network(
-                widget.imgURL,
-                width: 50.0,
-                height: MediaQuery.of(context).size.height,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Builder(
-              builder: (context) => IconButton(
-                icon: Icon(
-                  Icons.content_copy,
-                  semanticLabel: "Copy URL",
-                ),
-                color: Colors.white,
-                onPressed: () {
-                  Clipboard.setData(
-                      ClipboardData(text: widget.pin.id.hashCode.toString()));
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                    content: Text("URL copied to clipboard."),
-                  ));
-                },
-              ),
-            ),
+            visitedButton,
+            image,
+            copyURLButton,
           ],
         ),
         body: ReviewList(widget.pin, scrollController),
